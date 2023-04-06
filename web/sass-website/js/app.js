f9eac892066c9aa8ac604b6440fb1962bb581aa1
Aneesh <aneeshad2@gmail.com>
2023-04-06 14:11:40 +0530

    file

---
+++ web/sass-website/js/app.js
@@ -0,0 +1,20 @@
+const menuBtn=document.querySelector('.menu-btn');
+const menuImg=document.querySelector('.menu-btn-img');
+const menuExit=document.querySelector('.menu-exit-btn-img')
+
+const nav=document.querySelector('.nav');
+
+let showMenu=false;
+
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('open');
+    nav.classList.toggle('open2');
+});
+
+
+menuExit.addEventListener('click',()=>{
+    nav.classList.toggle('open2');
+})
+
+
+
